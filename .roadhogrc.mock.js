df5c7c3ff13217b7c82fcc72859b567404a7144e
mitoriko <mitoriko@icloud.com>
2018-02-07 20:26:19 +0800

    Close #3 完成用户登录接口对接

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -3,7 +3,6 @@ import { getRule, postRule } from './mock/rule';
 import { getMenuData } from './mock/menu';
 import { getActivities, getNotice, getFakeList } from './mock/api';
 import { getFakeChartData } from './mock/chart';
-import { imgMap } from './mock/utils';
 import { getProfileBasicData } from './mock/profile';
 import { getProfileAdvancedData } from './mock/profile';
 import { getNotices } from './mock/notices';
@@ -16,7 +15,7 @@ const noProxy = process.env.NO_PROXY === 'true';
 const proxy = {
   // 支持值为 Object 和 Array
   'GET /api/currentUser': {
-    $desc: "获取当前用户接口",
+    $desc: '获取当前用户接口',
     $params: {
       pageSize: {
         desc: '分页',
@@ -63,7 +62,7 @@ const proxy = {
     res.send({ message: 'Ok' });
   },
   'GET /api/tags': mockjs.mock({
-    'list|100': [{ name: '@city', 'value|1-100': 150, 'type|0-2': 1 }]
+    'list|100': [{ name: '@city', 'value|1-100': 150, 'type|0-2': 1 }],
   }),
   'GET /api/fake_list': getFakeList,
   'GET /api/fake_chart_data': getFakeChartData,
@@ -71,27 +70,27 @@ const proxy = {
   'GET /api/profile/advanced': getProfileAdvancedData,
   'POST /api/login/account': (req, res) => {
     const { password, userName, type } = req.body;
-    if(password === '888888' && userName === 'admin'){
+    if (password === '888888' && userName === 'admin') {
       res.setHeader('code', '0');
       res.setHeader('msg', 'success');
       res.send({
         status: true,
         type,
         currentAuthority: 'admin',
-        token: {token: 'admin1234',  userId: 'admin' },
+        token: { token: 'admin1234', userId: 'admin' },
       });
-      return ;
+      return;
     }
-    if(password === '123456' && userName === 'user'){
+    if (password === '123456' && userName === 'user') {
       res.setHeader('code', '0');
       res.setHeader('msg', 'success');
       res.send({
         status: true,
         type,
         currentAuthority: 'user',
-        token: {token: 'user1234',  userId: 'user' },
+        token: { token: 'user1234', userId: 'user' },
       });
-      return ;
+      return;
     }
     res.setHeader('code', '0');
     res.setHeader('msg', 'wrong');
@@ -99,7 +98,7 @@ const proxy = {
       status: false,
       type,
       currentAuthority: 'guest',
-      token: {token: 'user1234',  userId: 'user' },
+      token: { token: 'user1234', userId: 'user' },
     });
   },
   'POST /api/register': (req, res) => {
@@ -108,38 +107,38 @@ const proxy = {
   'GET /api/notices': getNotices,
   'GET /api/500': (req, res) => {
     res.status(500).send({
-      "timestamp": 1513932555104,
-      "status": 500,
-      "error": "error",
-      "message": "error",
-      "path": "/base/category/list"
+      timestamp: 1513932555104,
+      status: 500,
+      error: 'error',
+      message: 'error',
+      path: '/base/category/list',
     });
   },
   'GET /api/404': (req, res) => {
     res.status(404).send({
-      "timestamp": 1513932643431,
-      "status": 404,
-      "error": "Not Found",
-      "message": "No message available",
-      "path": "/base/category/list/2121212"
+      timestamp: 1513932643431,
+      status: 404,
+      error: 'Not Found',
+      message: 'No message available',
+      path: '/base/category/list/2121212',
     });
   },
   'GET /api/403': (req, res) => {
     res.status(403).send({
-      "timestamp": 1513932555104,
-      "status": 403,
-      "error": "Unauthorized",
-      "message": "Unauthorized",
-      "path": "/base/category/list"
+      timestamp: 1513932555104,
+      status: 403,
+      error: 'Unauthorized',
+      message: 'Unauthorized',
+      path: '/base/category/list',
     });
   },
   'GET /api/401': (req, res) => {
     res.status(401).send({
-      "timestamp": 1513932555104,
-      "status": 401,
-      "error": "Unauthorized",
-      "message": "Unauthorized",
-      "path": "/base/category/list"
+      timestamp: 1513932555104,
+      status: 401,
+      error: 'Unauthorized',
+      message: 'Unauthorized',
+      path: '/base/category/list',
     });
   },
   'POST /api/menu': getMenuData,
