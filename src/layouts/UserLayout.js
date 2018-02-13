d2bc780626306af99fcc3ca220d4a0bd3a3060ff
rainie_pang <1838529223@qq.com>
2018-02-13 22:34:04 +0800

    login logo

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -4,7 +4,7 @@ import DocumentTitle from 'react-document-title';
 import { Icon } from 'antd';
 import GlobalFooter from '../components/GlobalFooter';
 import styles from './UserLayout.less';
-import logo from '../assets/logo.svg';
+import logo from '../assets/logo.png';
 import { getRoutes } from '../utils/utils';
 
 const links = [{
@@ -21,7 +21,7 @@ const links = [{
   href: '',
 }];
 
-const copyright = <div>Copyright <Icon type="copyright" /> 2018 蚂蚁金服体验技术部出品</div>;
+const copyright = <div>Copyright <Icon type="copyright" /> 2018 蚂蚁岂止科技（大连）有限公司 </div>;
 
 class UserLayout extends React.PureComponent {
   getPageTitle() {
@@ -43,10 +43,10 @@ class UserLayout extends React.PureComponent {
               <div className={styles.header}>
                 <Link to="/">
                   <img alt="logo" className={styles.logo} src={logo} />
-                  <span className={styles.title}>Ant Design</span>
+                  
                 </Link>
               </div>
-              <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
+              
             </div>
             <Switch>
               {getRoutes(match.path, routerData).map(item =>
