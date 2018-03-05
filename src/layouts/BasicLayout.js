a811481bc543cee2f70b9c6d5e728c272854eb98
ming han <40271842@qq.com>
2018-03-05 20:27:47 +0800

    修改了Copyright

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -214,7 +214,7 @@ class BasicLayout extends React.PureComponent {
           <GlobalFooter
             copyright={
               <div>
-                Copyright <Icon type="copyright" /> 2017 壹立方（大连）信息技术有限公司出品
+                Copyright <Icon type="copyright" /> 2017 岂止科技（大连）有限公司出品
               </div>
             }
           />
