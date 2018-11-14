daad07b443eff108b661f9716b8753f489327482
Una <una_bella@qq.com>
2018-11-14 16:29:29 +0800

    test
    test

--- src/common/menuFunctionRouter.js
+++ src/common/menuFunctionRouter.js
@@ -134,7 +134,10 @@ export default function(dynamicWrapper,app){
       component: dynamicWrapper(app, ['settlementManagement'], () => import('../routes/SettlementManagement/incomeStore')),
     },
 
+  //
 
+
+    
 
   };
 };
